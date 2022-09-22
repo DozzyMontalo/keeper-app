1db215383f401d2c4a8269941926598f92217086
DozzyMontalo <dozienice@gmail.com>
2022-09-23 00:58:29 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Note from './Note'
+import Header from './header'
+import Footer from './footer'
+
+function App() {
+    return <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>;
+  };
+  
+  export default App;
+  
